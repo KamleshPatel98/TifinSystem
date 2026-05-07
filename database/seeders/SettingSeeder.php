20f97fd514193a860ea089532bb9b05c8806a7b9
KamleshPatel98 <kamleshp52170@gmail.com>
2026-05-07 11:45:01 +0530

    setting seeder

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'key_name' => 'app_name',
+                'value' => 'TifinSystem',
+            ],
+            [
+                'key_name' => 'app_email',
+                'value' => 'admin@gmail.com',
+            ],
+            [
+                'key_name' => 'app_phone',
+                'value' => '+91 1234567891',
+            ],
+            [
+                'key_name' => 'app_alt_phone',
+                'value' => '+91 1234567892',
+            ],
+            [
+                'key_name' => 'app_address',
+                'value' => 'Raipur, Chhattisgarh, India - 492001',
+            ],
+            [
+                'key_name' => 'app_footer_text',
+                'value' => '© 2026 TifinSystem. All rights reserved.',
+            ],
+            [
+                'key_name' => 'page_limit',
+                'value' => '15',
+            ],
+            [
+                'key_name' => 'app_url',
+                'value' => 'https://tifin.mywaydigitalsolutions.com',
+            ],
+        ];
+
+        foreach ($data as $row) {
+            Setting::firstOrCreate(
+                ['key_name' => $row['key_name']],
+                ['value' => $row['value']]
+            );
+        }
+    }
+}
